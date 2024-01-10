352736adfe395c3e342ff39a1d93d507134683b7
BorisHernan <89934200+BorisHernan@users.noreply.github.com>
2024-01-10 17:40:45 -0500

    ultimos detalles

--- alumnos.php
+++ alumnos.php
@@ -9,34 +9,38 @@ if (isset($_POST['buscar'])) {
     $dni_buscar = $_POST['dni'];
 
     if (strlen($dni_buscar) === 8 && is_numeric($dni_buscar)) {
+        $query = "SELECT * FROM estudiantes WHERE dni = ?";
+        
 
-        $query = "SELECT * FROM estudiantes WHERE TRIM(dni) = '$dni_buscar'";
-        
-        echo '<script>';
-        echo 'console.log("Consulta SQL: ' . $query . '");';
-        echo '</script>';
-        
-        $data = mysqli_query($conexion, $query);
+        $stmt = mysqli_prepare($conexion, $query);
+        mysqli_stmt_bind_param($stmt, "s", $dni_buscar);
+        mysqli_stmt_execute($stmt);
+
+        $data = mysqli_stmt_get_result($stmt);
 
         if (!$data) {
             die("Error al realizar la consulta: " . mysqli_error($conexion));
         }
 
-
         while ($consulta = mysqli_fetch_array($data)) {
             $resultados[] = $consulta;
         }
     } else {
-
         $error_message = "El DNI debe tener exactamente 8 dígitos.";
     }
 
-    echo "Consulta SQL: $query <br>";
 } 
 ?>
 
+
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -48,111 +52,141 @@ if (isset($_POST['buscar'])) {
     <link rel="stylesheet" href="css/owl.css">
     <link rel="stylesheet" href="css/animate.css">
     <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@700&display=swap" rel="stylesheet">
 </head>
-<body>
 
-<header class="header-area header-sticky">
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <nav class="main-nav">
-                    <a href="alumnos.php" class="logo">
-                    </a>
-                    <ul class="nav">
-                      <li><a href="alumnos.php" class="active">Asistencia De Entrada</a></li>
-                      <li><a href="#">Asistencia De Salida</a></li>
-                      <li><a href="#">Alumnos</a></li>
-                      <li><a href="#">Listado de Asistencias</a></li>
-                    </ul>   
-                </nav>
-            </div>
-        </div>
-    </div>
-</header>
+<body>
+<link rel="stylesheet" href="css/styles.css">
+<div class="fieldset">    
+<fieldset>
+    <legend id="fecha"></legend>
+    <h2 id="reloj"></h2>
+</fieldset>
+</div>  
+
+  <script src="js/ejercicio.js"></script>
+
+
+  <form method="post" class="text-center mb-3 containerform">
+    <label for="dni">Buscar por DNI (8 dígitos):</label>
+    <input type="text" name="dni" id="dni" pattern="[0-9]{8}" title="El DNI debe tener 8 dígitos" oninput="checkLength()" required>
+    <button class="btn btn-primary" type="submit" name="buscar" id="buscarBtn" style="display:none;">Buscar</button>
+</form>
+
+<script>
+    function checkLength() {
+        var dniInput = document.getElementById('dni');
+        var buscarBtn = document.getElementById('buscarBtn');
+
+        if (dniInput.value.length === 8) {
+            buscarBtn.click();
+
+        }
+    }
+</script>
+
 
 <div class="perfil">
     <h1 class="containerform text-center body">Información del Alumno / Registro de Asistencia </h1>
     
-    <!-- Formulario de búsqueda por DNI -->
-    <form method="post" class="text-center mb-3 containerform">
-        <label for="dni">Buscar por DNI (8 dígitos):</label>
-        <input type="text" name="dni" id="dni" pattern="[0-9]{8}" title="El DNI debe tener 8 dígitos" required>
-    </form>
-
-    <?php
-    if ($error_message !== '') {
-        echo '<p class="text-danger">' . $error_message . '</p>';
-    } else {
-        foreach ($resultados as $resultado) {
-    ?>      
-            <div class="resultados">
-            <div  class="label1">
-                <h5><strong>DNI:</strong><h5>
-                <p class="texto-grande1"><?php echo $resultado['dni']; ?></p>
+    <div id="resultadosContainer" class="resultados">
+        <?php
+        if ($error_message !== '') {
+            echo '<p class="text-danger">' . $error_message . '</p>';
+        } else {
+            foreach ($resultados as $resultado) {
+        ?>
+            <div class="d-flex justify-content-between">
+                <div class="card contenedor" style="width: 14rem;">
+                <h3 class="card separator"><strong>DNI </strong><h4>
+                
+                <h4 class="separator"><?php echo $resultado['dni']; ?></h4>
+                <h4 class="card separator" ><strong>NOMBRE COMPLETO </strong><h4>
+                    <h4 class="separator"><?php echo $resultado['apenom']; ?></h4>
                 </div>
-                <div  class="label2">
-                <h5><strong>NOMBRES COMPLETO :</strong><h5>
-                    <p class="card-text"><?php echo $resultado['apenom']; ?></p>
-                </div>
-                <div  class="label3">
-                <h5><strong>NIVEL EDUCATIVO:</strong></h5>
-                    <div class="texto-grande2">
+                <div class="card separator" style="width: 14rem;">
+                <h3 class="card separator"><strong>NIVEL EDUCATIVO </strong></h3>
+                    <div>
                         <?php
                             $nivel = $resultado['nivel'];
                             
                             if ($nivel == 'P') {
-                                echo '<p>Primaria</p>';
+                                echo '<h4 class="separator" >Primaria</h4>';
                             } elseif ($nivel == 'S') {
-                                echo '<p>Secundaria</p>';
+                                echo '<h4 class="separator" >Secundaria</h4>';
                             } else {
                                 // Manejar otros casos si es necesario
-                                echo '<p>Otro Nivel</p>';
+                                echo '<h4 class="separator">Otro Nivel</h4>';
                             }
                         ?>
                     </div>
-                </div>
-                <div  class="label4">
-                <h5><strong>GRADO :</strong><h5>
-                <p class="texto-grande1"><?php echo $resultado['grado']; ?></p>
-                </div>
-                <div  class="label5">
-                <label><strong>SECCIÓN :</strong></label>
-                <p><?php echo $resultado['seccion']; ?></p>
+                <h3 class="card separator"  ><strong>GRADO :</strong><h3>
+                <h4 class="separator" ><?php echo $resultado['grado']; ?></h4>
+                <h3 class="card separator"  ><strong>SECCIÓN :</strong></h3>
+                <h4 class="separator"><?php echo $resultado['seccion']; ?></h4>
                 </div>
 
                 <div class="card contenedor" style="width: 14rem;">
                 <img src="<?php echo $resultado['img'] ?>" class="card-img-top" alt="...">
-
-                    <!-- Agrega el ID del estudiante como un atributo de datos -->
-                <!--    <a class="btn btn-primary asistio" data-idalum="<?php echo $resultado['id']; ?>">Asistió</a>
-                   <a class="btn btn-primary no-asistio" data-idalum="<?php echo $resultado['id']; ?>">No Asistió</a>-->
+                <button class="btn btn-primary asistio" data-idalum="<?php echo $resultado['id']; ?>" id="buscarBtn1" style="display:none;">asitencia</button>
                 </div>
             </div>
-
+            </div>
     <?php
         }
     }
     ?>
 
+            <script>
+                function limpiarResultados() {
+                    buscarBtn1.click();
+                    document.getElementById('resultadosContainer').innerHTML = '';
+                }
+
+                setTimeout(limpiarResultados, 2000);
+            </script>
+
+
+
+            <script>
+                $(document).ready(function(){
+                    // Manejar clic en el botón "Asistió"
+                $(".asistio").click(function(){
+                    var idalum = $(this).data('idalum');
+
+                    $.ajax({
+                        type: 'POST',
+                        url: 'asist.php', 
+                        data: { idalum: idalum, asis: 'A' },
+                         dataType: 'json',
+                            success: function(response) {
+                             if (response.success) {
+                                console.log(response.message);
+                                    
+                            } else {
+                                console.error(response.message);
+                            }
+                        },
+                        error: function(xhr, status, error) {
+                            console.error('Error en la solicitud AJAX:', status, error);
+                        }
+                    });
+                });
+            });
+        </script>
+            
+
 </div>
 
-<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
 <script src="js/isotope.min.js"></script>
 <script src="js/owl-carousel.js"></script>
 <script src="js/counter.js"></script>
 <script src="js/custom.js"></script>
 
-<!-- Script para buscar automáticamente después de ingresar los 8 dígitos del DNI -->
-<script>
-    $(document).ready(function(){
-        $('#dni').on('input', function(){
-            if ($(this).val().length === 8) {
-                $('form').submit();
-            }
-        });
-    });
-</script>
 
 </body>
 </html>
